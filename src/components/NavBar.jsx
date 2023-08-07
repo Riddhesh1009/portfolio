26e7801c87837f93893dac765a5eed4cd660dbed
Riddhesh1009 <riddheshmalirm@gmail.com>
2023-08-07 20:06:09 +0530

    did changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import Link from "next/link"
 
+ 
+
 export default function NavBar() {
     return (
         <div className="bg-[#3F444B] h-[75px] fixed w-full z-40">
@@ -12,11 +14,11 @@ export default function NavBar() {
                     className="w-auto h-full object-contain sm:hover:scale-125 duration-500 ease-out"
                 />
                 <div className="invisible md:visible flex justify-center items-center text-black">
-                    <a href="/" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Home</a>
-                    <a href="/#about" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">About</a>
-                    <a href="/" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Projects</a>
-                    <a href="/" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Skills</a>
-                    <a href="/" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Contact</a>
+                    <Link offset={-75} href="/#home" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Home</Link>
+                    <Link offset={-75} href="/#about" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">About</Link>
+                    <Link offset={-75} href="/#projects" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Projects</Link>
+                    <Link offset={-75} href="/#skills" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Skills</Link>
+                    <Link offset={-75} href="/#contact" className="px-4 text-[#00C2CB] hover:scale-110 hover:mb-2 duration-500 font-medium ">Contact</Link>
                 </div>
             </div>
         </div>
